406a3a57be993061b4b0757048659bb9d3a2f5cd
Inessa Vasilevskaya <ivasilev@redhat.com>
2019-03-19 15:32:56 +0100

    add initial ntp->chrony converter tests
    Ntp configs to convert are stored in tests/data/ntpconfs dir,
    all test case files have the same sequential number.
    Expected chrony configs are stored in tests/data/chronyconfs.
    For each test case ntp config is taken, conversion is run and the
    result is compared with the expected result.

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,7 +1,11 @@
+import os
+
 from ntp2chrony import ntp2chrony
 
 NTP_CONF = "tests/data/ntp.conf"
 STEP_TICKERS = "tests/data/step_tickers"
+NTP_MATCH_DIR = "tests/data/ntpconfs/"
+CHRONY_MATCH_DIR = "tests/data/chronyconfs/"
 
 
 class TestConverter(object):
@@ -27,3 +31,42 @@ class TestConverter(object):
             if not any(num in range(x, y+1) for (x, y) in config.trusted_keys):
                 expected = '#' + expected
             assert expected in chrony_keys
+
+
+class TestConfigConversion(object):
+    def _do_match(self, expected_file, actual):
+        expected_lines = []
+        actual_lines = []
+        with open(expected_file) as f:
+            expected_lines = [l.strip() for l in f.readlines()
+                              if l.strip() and not l.strip().startswith('#')]
+        actual_lines = [l.strip() for l in actual.split('\n')
+                        if l.strip() and not l.strip().startswith('#')]
+        assert expected_lines == actual_lines
+
+    def _check_existance(self, fname, default=''):
+        if os.path.exists(fname):
+            return fname
+        return default
+
+    def test_match(self):
+
+        for f in [f for f in os.listdir(NTP_MATCH_DIR) if f.endswith('conf')]:
+            # get recorded actual result
+            num = f.split('.')[0].split('_')[0]
+            ntp_conf = os.path.join(NTP_MATCH_DIR, f)
+            step_tickers = self._check_existance(
+                    os.path.join(NTP_MATCH_DIR, '%s_step_tickers' % num))
+            config = ntp2chrony.NtpConfiguration('',
+                                                 ntp_conf,
+                                                 step_tickers=step_tickers)
+            potential_chrony_keys = os.path.join(CHRONY_MATCH_DIR, "%s_chrony.keys" % num)
+            actual_data = config.get_chrony_conf(chrony_keys_path=potential_chrony_keys)
+            expected_fname = os.path.join(CHRONY_MATCH_DIR, "%s_chrony.conf" % num)
+            # make sure recorded and generated configs match
+            self._do_match(expected_fname, actual_data)
+            actual_keys = config.get_chrony_keys()
+            expected_keys_file = self._check_existance(potential_chrony_keys)
+            # if keys are recorded or generated make sure they match
+            if actual_keys and expected_keys_file != '':
+                self._do_match(expected_keys_file, actual_keys)
